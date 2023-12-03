3cc3eba8dcd1747d6bf72af84d4b29152379aae3
hyunu02 <andrehwkc@gmail.com>
2023-12-03 19:11:35 -0300

    feat: Compare if-else and switch cases in worst case in C code

---
+++ performanceTestInC.c
@@ -0,0 +1,490 @@
+#include <stdio.h>
+#include <time.h>
+
+#define ITERATIONS 30
+
+int ifWith2Cases(int input) {
+    if(input == 0) {
+        return 0;
+    } else if(input == 1) {
+        return 0;
+    }
+    return 1;
+}
+
+int ifWith3Cases(int input) {
+    if(input == 0) {
+        return 0;
+    } else if(input == 1) {
+        return 0;
+    } else if(input == 2) {
+        return 0;
+    }
+    return 1;
+}
+
+int ifWith5Cases(int input) {
+    if(input == 0) {
+        return 0;
+    } else if(input == 1) {
+        return 0;
+    } else if(input == 2) {
+        return 0;
+    } else if(input == 3) {
+        return 0;
+    } else if(input == 4) {
+        return 0;
+    }
+    return 1;
+}
+
+int ifWith8Cases(int input) {
+    if (input == 0) {
+        return 0;
+    } else if (input == 1) {
+        return 0;
+    } else if (input == 2) {
+        return 0;
+    } else if (input == 3) {
+        return 0;
+    } else if (input == 4) {
+        return 0;
+    } else if (input == 5) {
+        return 0;
+    } else if (input == 6) {
+        return 0;
+    } else if (input == 7) {
+        return 0;
+    } 
+    return 1;
+}
+
+int ifWith10Cases(int input) {
+    if (input == 0) {
+        return 0;
+    } else if (input == 1) {
+        return 0;
+    } else if (input == 2) {
+        return 0;
+    } else if (input == 3) {
+        return 0;
+    } else if (input == 4) {
+        return 0;
+    } else if (input == 5) {
+        return 0;
+    } else if (input == 6) {
+        return 0;
+    } else if (input == 7) {
+        return 0;
+    } else if (input == 8) {
+        return 0;
+    } else if (input == 9) {
+        return 0;
+    }
+    return 1;
+}
+
+int ifWith30Cases(int input) {
+    if (input == 0) {
+        return 0;
+    } else if (input == 1) {
+        return 0;
+    } else if (input == 2) {
+        return 0;
+    } else if (input == 3) {
+        return 0;
+    } else if (input == 4) {
+        return 0;
+    } else if (input == 5) {
+        return 0;
+    } else if (input == 6) {
+        return 0;
+    } else if (input == 7) {
+        return 0;
+    } else if (input == 8) {
+        return 0;
+    } else if (input == 9) {
+        return 0;
+    } else if (input == 10) {
+        return 0;
+    } else if (input == 11) {
+        return 0;
+    } else if (input == 12) {
+        return 0;
+    } else if (input == 13) {
+        return 0;
+    } else if (input == 14) {
+        return 0;
+    } else if (input == 15) {
+        return 0;
+    } else if (input == 16) {
+        return 0;
+    } else if (input == 17) {
+        return 0;
+    } else if (input == 18) {
+        return 0;
+    } else if (input == 19) {
+        return 0;
+    } else if (input == 20) {
+        return 0;
+    } else if (input == 21) {
+        return 0;
+    } else if (input == 22) {
+        return 0;
+    } else if (input == 23) {
+        return 0;
+    } else if (input == 24) {
+        return 0;
+    } else if (input == 25) {
+        return 0;
+    } else if (input == 26) {
+        return 0;
+    } else if (input == 27) {
+        return 0;
+    } else if (input == 28) {
+        return 0;
+    } else if (input == 29) {
+        return 0;
+    }
+    return 1;
+}
+
+int switchWith2Cases(int input) {
+    switch(input) {
+        case 0:
+            return 0;
+            break;
+        case 1:
+            return 0;
+            break;
+        default:
+            return 1;
+    }
+}
+
+int switchWith3Cases(int input) {
+    switch(input) {
+        case 0:
+            return 0;
+            break;
+        case 1:
+            return 0;
+            break;
+        case 2:
+            return 0; 
+            break;   
+        default:
+            return 1;
+    }
+}
+
+int switchWith5Cases(int input) {
+    switch(input) {
+        case 0:
+            return 0;
+            break;
+        case 1:
+            return 0;
+            break;
+        case 2:
+            return 0;  
+            break;
+        case 3:
+            return 0;
+            break;
+        case 4:
+            return 0; 
+            break;   
+        default:
+            return 1;
+    }
+}
+
+int switchWith8Cases(int input) {
+    switch(input) {
+        case 0:
+            return 0;
+            break;
+        case 1:
+            return 0;
+            break;
+        case 2:
+            return 0;  
+            break;
+        case 3:
+            return 0;
+            break;
+        case 4:
+            return 0;
+            break;
+        case 5:
+            return 0;
+            break;
+        case 6:
+            return 0;
+            break;
+        case 7:
+            return 0; 
+            break;
+        default:
+            return 1;
+    }
+}
+
+int switchWith10Cases(int input) {
+    switch(input) {
+        case 0:
+            return 0;
+            break;
+        case 1:
+            return 0;
+            break;
+        case 2:
+            return 0;  
+            break;
+        case 3:
+            return 0;
+            break;
+        case 4:
+            return 0;
+            break;
+        case 5:
+            return 0; 
+            break; 
+        case 6:
+            return 0;
+            break;
+        case 7:
+            return 0;
+            break;
+        case 8:
+            return 0;
+            break;
+        case 9:
+            return 0;
+            break;
+        default:
+            return 1;
+    }
+}
+
+int switchWith30Cases(int input) {
+    switch(input) {
+        case 0:
+            return 0;
+            break;
+        case 1:
+            return 0;
+            break;
+        case 2:
+            return 0;  
+            break;
+        case 3:
+            return 0;
+            break;
+        case 4:
+            return 0;
+            break;
+        case 5:
+            return 0; 
+            break; 
+        case 6:
+            return 0;
+            break;
+        case 7:
+            return 0;
+            break;
+        case 8:
+            return 0;
+            break;
+        case 9:
+            return 0;
+            break;
+        case 10:
+            return 0;
+            break;
+        case 11:
+            return 0;
+            break;
+        case 12:
+            return 0;  
+            break;
+        case 13:
+            return 0;
+            break;
+        case 14:
+            return 0;
+            break;
+        case 15:
+            return 0; 
+            break; 
+        case 16:
+            return 0;
+            break;
+        case 17:
+            return 0;
+            break;
+        case 18:
+            return 0;
+            break;
+        case 19:
+            return 0;
+            break;
+        case 20:
+            return 0;
+            break;
+        case 21:
+            return 0;
+            break;
+        case 22:
+            return 0;  
+            break;
+        case 23:
+            return 0;
+            break;
+        case 24:
+            return 0;
+            break;
+        case 25:
+            return 0; 
+            break; 
+        case 26:
+            return 0;
+            break;
+        case 27:
+            return 0;
+            break;
+        case 28:
+            return 0;
+            break;
+        case 29:
+            return 0;
+            break;
+        default:
+            return 1;
+    }
+}
+
+void loopIfCaseTenMillionTimes(int cases) {
+    int (*functionPointer)(int);
+    int input;
+    clock_t start, end;
+    double elapsed = 0.0;
+    switch(cases) {
+        case 2:
+            functionPointer = ifWith2Cases;
+            input = 1;
+            break;
+        case 3:
+            functionPointer = ifWith3Cases;
+            input = 2;
+            break;
+        case 5:
+            functionPointer = ifWith5Cases;
+            input = 4;
+            break;
+        case 8:
+            functionPointer = ifWith8Cases;
+            input = 7;
+            break;
+        case 10:
+            functionPointer = ifWith10Cases;
+            input = 9;
+            break;
+        case 30:
+            functionPointer = ifWith30Cases;
+            input = 29;
+            break;
+        default:
+            break;
+    }
+
+    for(int i = 0; i < ITERATIONS; i++) {
+        start = clock();
+        for(int j = 0; j < 10000000; j++) {
+            functionPointer(input);
+        }
+        end = clock();
+        elapsed += (double)(end - start)/CLOCKS_PER_SEC;
+    }
+    printf("Average spent time: %.3f seconds, for if/else case with %d cases, average of %d iterations.\n", elapsed/ITERATIONS, cases, ITERATIONS);
+}
+
+void loopSwitchCaseTenMillionTimes(int cases) {
+    int (*functionPointer)(int);
+    int input;
+    clock_t start, end;
+    double elapsed = 0.0;
+    switch(cases) {
+        case 2:
+            functionPointer = switchWith2Cases;
+            input = 1;
+            break;
+        case 3:
+            functionPointer = switchWith3Cases;
+            input = 2;
+            break;
+        case 5:
+            functionPointer = switchWith5Cases;
+            input = 4;
+            break;
+        case 8:
+            functionPointer = switchWith8Cases;
+            input = 7;
+            break;
+        case 10:
+            functionPointer = switchWith10Cases;
+            input = 9;
+            break;
+        case 30:
+            functionPointer = switchWith30Cases;
+            input = 29;
+            break;
+        default:
+            break;
+    }
+
+    for(int i = 0; i < ITERATIONS; i++) {
+        start = clock();
+        for(int j = 0; j < 10000000; j++) {
+            functionPointer(input);
+        }
+        end = clock();
+        elapsed += (double)(end - start)/CLOCKS_PER_SEC;
+    }
+    printf("Average spent time: %.3f seconds, for switch case with %d cases, average of %d iterations.\n", elapsed/ITERATIONS, cases, ITERATIONS);    
+}
+
+int main(void) {
+    // AVAILABLE CASES: 2, 3, 5, 8, 10, 30
+
+    loopIfCaseTenMillionTimes(2);
+    loopSwitchCaseTenMillionTimes(2);
+
+    printf("\n");
+
+    loopIfCaseTenMillionTimes(3);
+    loopSwitchCaseTenMillionTimes(3);
+
+    printf("\n");
+
+    loopIfCaseTenMillionTimes(5);
+    loopSwitchCaseTenMillionTimes(5);
+
+    printf("\n");
+
+    loopIfCaseTenMillionTimes(8);
+    loopSwitchCaseTenMillionTimes(8);
+
+    printf("\n");
+
+    loopIfCaseTenMillionTimes(10);
+    loopSwitchCaseTenMillionTimes(10);
+
+    printf("\n");
+
+    loopIfCaseTenMillionTimes(30);
+    loopSwitchCaseTenMillionTimes(30);
+
+    return 0;
+}
